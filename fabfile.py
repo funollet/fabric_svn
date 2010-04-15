b373a0743f7407ca9dacc5a77ad6fbfae27d0bc6
Jordi Funollet <jordi.f@ati.es>
2010-04-16 01:45:34 +0200

    added description and license

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,31 @@
 # -*- coding: utf-8 -*-
-# fabmodules/subversion.py
+# fabfile.py
+"""Fabric tasks for managing SVN repositories.
+
+Creates repositories with a minimal tree; configure Apache to serve repository
+via WebDAV; manages users.
+
+Examples:
+
+    # fab new:myproject
+    # fab changeusers:myproject,"andy bert carl"
 """
-"""
+
+# Copyright 2009 Jordi Funollet <jordi.f@ati.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 from fabric.api import *
 from fabric.contrib.files import *
